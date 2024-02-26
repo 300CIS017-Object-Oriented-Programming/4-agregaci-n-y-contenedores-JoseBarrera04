b2d590c2b9b59e29e45872c14b3331dc3e89a0ca
Jose Barrera Ramos <103261177+JoseBarrera04@users.noreply.github.com>
2024-02-25 23:53:46 -0500

    Update Refugio.h

--- Refugio.h
+++ Refugio.h
@@ -6,6 +6,7 @@
 #include <iterator>
 #include <unordered_map>
 #include "Perro.h"
+#include "Propietario.h"
 
 using std::string;
 using std::cout;
@@ -20,22 +21,27 @@ private:
     string nombre;
     int acumPerros;
     vector<Perro*> bdPerros;
-    unordered_map<int, Perro*> mapaPerros;
+    unordered_map<int,Perro*> mapaPerros;
+
 public:
     Refugio()= default;
     ~Refugio();
     Refugio(string nombre);
 
-
+    void agregarPerro(int id, int edad, string nombre, string raza, string tamanio, string color);
     void recibirPerro(int edad, string nombre, string raza, string tamanio, string color);
     void mostrarPerros();
     bool buscarPerro(int id);
-    void listarPerrosPorEdad(int edad);
-    void darPerroEnAdopcion(int id);
+    void listarPerrosPorEdad(int edad,int edadMayor);
+    void darPerroEnAdopcion(int id, int idPropietario);
 
     int getAcumPerros();
     string getNombre();
     void setNombre(string nombre);
+    vector<Perro *> getBdPerros();
+    unordered_map<int, Perro *> getMapaPerros();
+
+    Propietario *encontrarPropietario(int propietario);
 };
 
 #endif

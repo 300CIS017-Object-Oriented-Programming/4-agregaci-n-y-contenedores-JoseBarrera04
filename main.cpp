893caeb5ce47dc940fd2b03090ca9e5385ad9ebc
Jose Barrera Ramos <103261177+JoseBarrera04@users.noreply.github.com>
2024-02-25 23:50:33 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Perro.h"
 #include "Refugio.h"
+#include "Propietario.h"
 
 using std::cout;
 using std::endl;
@@ -8,24 +9,78 @@ using std::cin;
 using std::string;
 using std::getline;
 
-void primeraOpcion(Refugio* refugio) {
-    // TO DO
+void primeraOpcion(Refugio* refugio,int id) {
+    int edad;
+    string nombre, raza, tamanio, color;
+    cout << "Escribe los datos del perro" << endl << endl;
+
+    cout << "Edad:" << endl;
+    cin >> edad;
+    cout << "Nombre: " << endl;
+    cin >> nombre;
+    cout << "Raza:" << endl;
+    cin>>raza;
+    cout << "Tamanio:" << endl;
+    cin >> tamanio;
+    cout << "Color" << endl;
+    cin >> color;
+    refugio->agregarPerro(id,edad,nombre, raza, tamanio, color);
 }
 
 void terceraOpcion(Refugio* refugio) {
-    // TO DO
+    int id;
+    cout << "ID del Perro: " << endl;
+    cin >> id;
+    bool found = refugio->buscarPerro(id);
+
+    cout << (found ? "El perro está en el refugio" : "El perro no está en el refugio") << endl;
 }
 
 void cuartaOpcion(Refugio* refugio) {
-    // TO DO
+    int edadMinima, edadMaxima;
+    cout << "Edad Minima: "<< endl;
+    cin >> edadMinima;
+    cout << "Edad Maxima: "<< endl;
+    cin >> edadMaxima;
+    refugio->listarPerrosPorEdad(edadMinima,edadMaxima);
 }
 
 void quintaOpcion(Refugio* refugio) {
-    // TO DO
+    int id, idPropietario;
+    cout << "Escribe el id del perro que desas dar en adopcion: " << endl;
+    cin >> id;
+    cout << "Escribe el id del propietario: " << endl;
+    cin >> idPropietario;
+    refugio->darPerroEnAdopcion(id, idPropietario);
 }
 
-void mostrarMenu(Refugio* refugio) {
+void sextaOpcion(Propietario* propietario, Refugio* refugio){
+    int idPerro, idPropietario;
+    cout << "Escribe el id del perro: "<< endl;
+    cin >> idPerro;
+    cout << "Escribe el id del propietario" << endl;
+    cin >> idPropietario;
+}
+
+void septimaOpcion(Propietario* propietario){
+    int idPerro, idPropietario;
+    cout << "Escribe el id del perro: "<< endl;
+    cin >> idPerro;
+    cout << "Escribe el id del propietario" << endl;
+    cin >> idPropietario;
+    propietario->borrarPerroPropietario(idPerro,idPropietario);
+}
+void octavaOpcion(Propietario* propietario, Refugio* refugio){
+    int idPerro;
+    cout << "Escribe el id del perro: "<< endl;
+    cin >> idPerro;
+    string text = propietario->dondeEstaElPerro(idPerro,refugio);
+    cout << text << endl;
+
+}
+void mostrarMenu(Refugio* refugio, Propietario* propietario) {
     int opc = 0;
+    int id = 1;
     do
     {
         cout << "\n**** Bienvenidos al refugio - " << refugio->getNombre() << " ****\n";
@@ -34,38 +89,74 @@ void mostrarMenu(Refugio* refugio) {
         cout << "3. Buscar un perro\n";
         cout << "4. Listar perros por edad\n";
         cout << "5. Dar un perro en adopcion\n";
+        cout << "6. Adoptar un perro\n";
+        cout << "7. Borrar perro fallecido\n";
+        cout << "8. Saber donde esta el perro\n";
         cout << "0. Salir\n" << endl;
 
         cin >> opc;
 
         switch (opc)
         {
-            case 1:
-                primeraOpcion(refugio);
+            case 0: {
+                cout << "Saliendo del programa\n";
                 break;
-            case 2:
+            }
+
+            case 1: {
+                primeraOpcion(refugio, id);
+                id++;
+                break;
+            }
+
+            case 2: {
                 refugio->mostrarPerros();
                 break;
-            case 3:
+            }
+
+            case 3: {
                 terceraOpcion(refugio);
                 break;
-            case 4:
+            }
+
+            case 4: {
                 cuartaOpcion(refugio);
                 break;
-            case 5:
+            }
+
+            case 5: {
                 quintaOpcion(refugio);
                 break;
+            }
+
+            case 6: {
+                sextaOpcion(propietario, refugio);
+                break;
+            }
+
+            case 7: {
+                septimaOpcion(propietario);
+                break;
+            }
+
+            case 8: {
+                octavaOpcion(propietario,refugio);
+                break;
+            }
+
             default:
+                cout << "Opcion no valida\n";
                 break;
         }
     } while (opc != 0);
 }
 
 int main() {
-    Refugio* pRefugio= new Refugio("Mi mascota feliz");
-    mostrarMenu(pRefugio);
+    Refugio* pRefugio= new Refugio("El Arca de Noe");
+    Propietario* propietario = new Propietario(1, "Juan");
 
-    // Debo usar el delete explicitamente cuando tengo objetos creados en el heap, ya que estos no se eliminan automáticamente.
+    mostrarMenu(pRefugio,propietario);
+
     delete pRefugio;
     return 0;
 }

4bd52108905a9d7b46f79d5b15575449095abb7f
Jose Barrera Ramos <103261177+JoseBarrera04@users.noreply.github.com>
2024-02-25 23:54:05 -0500

    Update Refugio.cpp

--- Refugio.cpp
+++ Refugio.cpp
@@ -1,13 +1,11 @@
 #include "Refugio.h"
-
-// To Do: organizar todas las entradas y salidas de datos para que esta clase no tenga
-// interacción con la consola, esa tarea será sólo responsabilidad del main
+#include "Perro.h"
+#include "Propietario.h"
+#include <vector>
 
 Refugio::Refugio(string nombre) : acumPerros(1), nombre(nombre) {}
 
 Refugio::~Refugio() {
-    // Recorremos el vector para eliminar los objetos. No sería necesario recorrer el map ya que se almacenaban los
-    // mismos objetos y la referencia ya no existiría.
     for (int indexVector = 0; indexVector < this->bdPerros.size(); indexVector++) {
         delete this->bdPerros[indexVector];
     }
@@ -15,44 +13,90 @@ Refugio::~Refugio() {
     cout << "\n¡Gracias por visitarnos!" << endl;
 }
 
-// TO DO: Completar
-void Refugio::recibirPerro(int edad, string nombre, string raza, string tamanio, string color) {
-    // Creo el objeto perro que voy a pasar a mi contenedor.
-    Perro* pPerroTemp = new Perro(this->acumPerros, edad, nombre, raza, tamanio, color); // Creado en el heap
 
-    // TO DO: Agregar el objeto al final de un vector.
-    this->bdPerros.push_back(pPerroTemp);
-
-    // TO DO: Agregar el objeto a un map de dos formas: Definiendo manualmente la llave o creando un par llave + referencia.
-    //Ejemplo1: this-mapita[id] = referencia;
-    this->mapaPerros[pPerroTemp->getId()] = pPerroTemp;
-
-    //Ejemplo2: this->mapita.insert(make_pair(this->soyElID, soyLaReferencia));
-    this->mapaPerros.insert(make_pair(pPerroTemp->getId(), pPerroTemp));
-
-    this->acumPerros++; // incrementa el contador de perros
+void Refugio::agregarPerro(int id, int edad, string nombre, string raza, string tamanio, string color){
+    Perro* perroP = new Perro(id, edad, nombre, raza, tamanio, color);
+    bdPerros.push_back(perroP);
+    mapaPerros.insert(make_pair(perroP->getId(),perroP));
 }
 
-// TO DO: Completar
+void Refugio::recibirPerro(int edad, string nombre, string raza, string tamanio, string color) {
+    Perro* pPerroTemp = new Perro(this->acumPerros, edad, nombre, raza, tamanio, color);
+
+    bdPerros.push_back(pPerroTemp);
+
+    mapaPerros.insert(make_pair(this->acumPerros, pPerroTemp));
+
+    this->acumPerros++;
+}
+
 void Refugio::mostrarPerros() {
     vector<Perro*>::iterator itVector;
     unordered_map<int, Perro*>::iterator itMap;
 
     cout << "Los perros en el refugio son:\n";
-    // Recorrer el vector
     cout << "\nVECTOR\n";
+
     for (itVector = bdPerros.begin(); itVector != bdPerros.end(); ++itVector) {
-        cout << "Perro ID: " << (*itVector)->getId() << ", Nombre: " << (*itVector)->getNombre() << ", Edad: " << (*itVector)->getEdad() << endl;
+        cout << "Id: "<< (*itVector)->getId() << endl << "Edad: "<<(*itVector)->getEdad() << endl << "Nombre: " << (*itVector)->getNombre() << endl << "Raza: " << (*itVector)->getRaza() << endl << "Tamanio: " << (*itVector)->getTamanio() << endl << "Color: " << (*itVector)->getColor() << endl;
     }
 
-// Recorrer el map
     cout << "\nMAP\n";
     for (itMap = mapaPerros.begin(); itMap != mapaPerros.end(); ++itMap) {
-        cout << "Perro ID: " << itMap->second->getId() << ", Nombre: " << itMap->second->getNombre() << ", Edad: " << itMap->second->getEdad() << endl;
+        cout << "Id: " << itMap->first << endl << "Nombre: " << itMap->second->getNombre() << endl << "Edad: " << itMap->second->getEdad() << endl << "Raza: " << itMap->second->getRaza() << endl << "Tamanio: " << itMap->second->getTamanio() << endl << "Color: " << itMap->second->getColor() << endl;
     }
 
 }
 
+bool Refugio::buscarPerro(int id) {
+    bool found = false;
+    int flag = 0;
+
+    vector<Perro*>::iterator itVector;
+    for (itVector = bdPerros.begin(); itVector != bdPerros.end() && flag == 0; ++itVector) {
+        if ((*itVector)->getId() == id) {
+            found = true;
+            flag = 1;
+        }
+    }
+    return found;
+}
+
+void Refugio::listarPerrosPorEdad(int edadMenor, int edadMayor) {
+    vector<Perro*>::iterator itVector;
+    for (itVector = bdPerros.begin(); itVector != bdPerros.end(); ++itVector) {
+        if ((*itVector)->getEdad() <= edadMayor && (*itVector)->getEdad() >= edadMenor) {
+            cout << "Id: " << (*itVector)->getId() << " Edad: " << (*itVector)->getEdad() << " Nombre: " << (*itVector)->getNombre() << " Raza: " << (*itVector)->getRaza() << " Tamanio: " << (*itVector)->getTamanio() << " Color: " << (*itVector)->getColor() << endl;
+        }
+    }
+}
+
+
+void Refugio::darPerroEnAdopcion(int idPerro, int idPropietario) {
+    int found = 0;
+    vector<Perro*>::iterator itPerro;
+
+    for (itPerro = bdPerros.begin(); itPerro != bdPerros.end() && found == 0; ++itPerro) {
+        if ((*itPerro)->getId() == idPerro) {
+            found = 1;
+            Propietario* propietario = encontrarPropietario(idPropietario);
+            if (propietario != nullptr) {
+                //Aqui me bugie
+                //propietario->asociarPerro(*itPerro);
+                bdPerros.erase(itPerro);
+            } else {
+                cout << "Propietario no encontrado." << endl;
+            }
+        }
+    }
+    if (found == 0) {
+        cout << "Perro no encontrado." << endl;
+    }
+}
+
+//Propietario *Refugio::encontrarPropietario(int propietario) {
+//    return propietarioTemp;
+//}
 
 int Refugio::getAcumPerros() {
     return this->acumPerros;
@@ -64,4 +108,12 @@ string Refugio::getNombre() {
 
 void Refugio::setNombre(string nombre) {
     this->nombre = nombre;
+}
+
+vector<Perro *> Refugio::getBdPerros()  {
+    return bdPerros;
+}
+
+unordered_map<int, Perro *> Refugio::getMapaPerros()  {
+    return mapaPerros;
 }
